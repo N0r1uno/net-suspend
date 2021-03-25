a836526f13d695cd2ef942278f961dcadcd36a0d
N0r1uno <noriunomakusui@gmail.com>
2021-03-25 18:51:28 +0100

    various changes

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # net-suspend
+Simple utility which puts your computer to sleep after some time if there is no activity on specified ports. Very handy in combination with automated WoL.
+
+## Notice
+* ❗️**socket statistics (ss)** must be installed
+* if not run as systemd service, make sure it runs as root
+* only tested on Ubuntu Server 20.04 LTS and Manjaro, no gurantees that it will work anywhere else whatsoever
+
+## Getting started
+Either download the [latest release]() binary or build it yourself (`make`).
+
+## Usage
+./netsusp -l -d ***delay*** -***protocol*** ***port*** [...]
+  * -l: write log file to `/var/log/netsusp.log` (optional)
+  * delay: time in minutes until suspend after inactivity\
+  * protocol: u(dp)/t(cp)\
+  * port: 0-65535
+
+eg: **netsusp -d 30 -t 25565 -t 22**
+will not suspend if there are any established tcp connections on port 25565(⛏💎) and 22(ssh).
+If idling for 30 minutes straight, the system will suspend.
+
+## Run as systemd service (recommended)
+* change the highlighted content of the given netsusp.service file to fit your needs
+* copy the edited file to `/lib/systemd/system/`.
+* then issue the following:\
+`systemctl daemon-reload`\
+`systemctl enable netsusp.service`\
+`systemctl start netsusp.service`
--- netsusp.c
+++ netsusp.c
@@ -3,71 +3,86 @@
 #include <unistd.h>
 #include <getopt.h>
 #include <signal.h>
+#include <ctype.h>
 #include <time.h>
 
-struct entry
-{
+char _log = 0;
+
+struct entry {
     int port;
     char protocol;
 };
 
-int established(const struct entry con)
-{
+int only_digits(const char *s) {
+    while (*s) {
+        if (*s != '\n' && !isdigit(*s)) return 0;
+        s++;
+    }
+    return 1;
+}
+
+int established(const struct entry con) {
     int result = -1;
-    char buf[64];
-    sprintf(buf, "netstat -%c | grep -c ':%i'", con.protocol, con.port);
+    char buf[64] = {'\0'};
+    sprintf(buf, "ss -%c | grep -c ':%i'", con.protocol, con.port);
     FILE *fp = popen(buf, "r");
     if (!fp)
         return -1;
-    if (fgets(buf, 64, fp))
+     if (fgets(buf, 8, fp) && only_digits(buf))
         result = atoi(buf);
     pclose(fp);
     return result;
 }
 
-void f_log(const char *msg)
-{
+void f_log(const char *msg) {
+    if (!_log) return;
     char tstamp[26];
     time_t timer = time(NULL);
     struct tm *tm_info = localtime(&timer);
     strftime(tstamp, 26, "%Y-%m-%d %H:%M:%S", tm_info);
     FILE *f = fopen("/var/log/netsusp.log", "a");
+    if (!f) {
+        fprintf(stderr, "An error occurred while trying to write to the logfile. (root rights required!)\n");
+        exit(EXIT_FAILURE);
+    }
     fprintf(f, "[%s]:%s\n", tstamp, msg);
     fclose(f);
 }
 
-void help()
-{
-    printf("usage:\tnetsusp -d <delay> -<protocol> <port> [...]"
+void help() {
+    printf("usage:\tnetsusp -l -d <delay> -<protocol> <port> [...]"
+           "\n  l:\t\twrite logs to /var/log/netsusp.log (optional)"
            "\n  delay:\ttime in minutes until suspend after inactivity"
            "\n  protocol:\tu(dp)/t(cp)\n  port:\t\t0-65535\n"
-           "\neg: netsusp -d=30 -t:25565 -u:8192\n");
+           "\neg: netsusp -d 30 -t 25565 -u 8192\n");
     exit(EXIT_FAILURE);
 }
 
-char r = 1;
-void term()
-{
-    r = 0;
+void term() {
+    f_log("Exit.");
+    exit(EXIT_SUCCESS);
 }
 
-int main(int argc, char **argv)
-{
-    if (argc < 5 || !(argc % 2))
+int main(int argc, char **argv) {
+    if (argc < 5)
         help();
 
-    const int l = (argc - 3) / 2;
+    const int l = (argc - (2 + !(argc % 2))) / 2;
     int opt, i = 0, delay = -1;
     struct entry e[l];
     opterr = 0;
 
-    while ((opt = getopt(argc, argv, "d:u:t:")) != -1)
-    {
-        if (opt == 'd')
+    while ((opt = getopt(argc, argv, "d:u:t:l")) != -1) {
+        if (opt == 'd') {
+            if (!only_digits(optarg))
+                help();
             delay = atoi(optarg);
-        else if (opt == 't' || opt == 'u')
-        {
+        } else if (opt == 'l')
+            _log = 1;
+        else if (opt == 't' || opt == 'u') {
             e[i].protocol = opt;
+            if (!only_digits(optarg))
+                help();
             e[i].port = atoi(optarg);
             if (e[i].port < 0 || e[i].port > 65535)
                 help();
@@ -82,7 +97,7 @@ int main(int argc, char **argv)
 
     for (i = 0; i < l; i++)
         if (established(e[i]) == -1) {
-            fprintf(stderr, "An error occurred while fetching connections with netstat.\n");
+            fprintf(stderr, "An error occurred while fetching connections with socket statistics(ss).\n");
             return EXIT_FAILURE;
         }
 
@@ -93,9 +108,12 @@ int main(int argc, char **argv)
 
     f_log("Init.");
 
+    printf("<--summary-->\ndelay=%imin%s\n", delay, (_log)?"\nlog enabled":"");
+    for (int i = 0; i < l; i++)
+        printf("%i.  %c:%i\n", i+1, e[i].protocol, e[i].port);
+
     int active, susp = 0;
-    while (r)
-    {
+    while (1) {
         for (i = active = 0; i < l; active += established(e[i++]));
         if (active <= 0) {
             if (susp >= delay) {
@@ -113,6 +131,5 @@ int main(int argc, char **argv)
         sleep(60);
     }
 
-    f_log("Exit.");
     return EXIT_SUCCESS;
 }
